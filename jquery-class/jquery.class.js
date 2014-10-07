cb317f70b63b1333a95428ef19d1e3cdc390d066
Alexandre Dionne <EyeCode@iMac-de-Alexandre.local>
2014-10-07 10:30:37 -0400

    Refactored namespacing / init call

--- jquery-class/jquery.class.js
+++ jquery-class/jquery.class.js
@@ -1,41 +1,12 @@
-$.Class = function(parent) {
-    var classPointer = function (obj, namespace) {
-        return namespace.length > 1 ? classPointer(obj[namespace.shift()], namespace) : obj;
-    }, definition = {}, cls = function(){};
-
-    if (arguments.length == 1 && typeof parent == 'object')
-        definition = parent;
-
-    if (arguments.length > 2) {
-        for(var i = 0; i < arguments.length - 1; i++) {
-            if (typeof arguments[i].prototype.namespace == 'undefined')
-                throw new Error('Missing namespace plugin loaded at ' + i + 'position');
-
-            if (arguments[i].prototype.namespace == "root") {
-                $.extend(true, cls.prototype, arguments[i].prototype);
-            } else {
-                var pointer = classPointer(cls.prototype, arguments[i].prototype.namespace.split("."));
-                pointer[arguments[i].prototype.namespace.split(".").pop()] = arguments[i].prototype;
-                if (arguments[i].prototype.namespace.split(".").length == 1)
-                    window[arguments[i].prototype.namespace] = pointer[arguments[i].prototype.namespace];
-            }
-        }
-        parent = cls;
-    }
-
-    var _parent = {};
-
-    if (typeof parent === 'function') {
-        for (var key in parent.prototype) {
-            _parent[key] = parent.prototype[key];
-        }
-
-        parent = $.extend(parent, parent.prototype);
-        definition = $.extend(parent, definition);
-    }
+$.Class = function(definition) {
+    if (arguments.length !== 1 || typeof definition !== 'object')
+        throw new Error('Wrong Class definition provided');
 
     var Class = function() {
-        if (definition.initialize) definition.initialize.apply(this, arguments);
+        $.each(definition, function(key, value){
+            if (/^init/.test(key))
+                definition[key].apply(this, []);
+        });
     };
 
     Class.prototype = definition;
@@ -47,6 +18,21 @@ $.Class = function(parent) {
                 function(def, cons) { return def[cons]}, this, definition.consts, constant
             );
         }
+
+    if (typeof definition.namespace !== 'undefined') {
+        var routing    = window,
+            namespaces = definition.namespace.split('.'),
+            className  = namespaces.pop();
+
+        $.each(namespaces, function(idx, namespace) {
+            if (typeof routing[namespace] !== 'function') {
+                routing[namespace] = function(){};
+            }
+            routing = routing[namespace].prototype;
+        });
+
+        routing[className] = Class;
+    }
 
     return Class;
 };
